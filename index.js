624f64ea4464038c48ee0f8717e310b5a09f3a63
Zach <zawood11@gmail.com>
2021-10-16 16:17:08 -0400

    commit tree 9f193a0b87fa37d9bf88f52cadd4d6b7e7069864

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.getElementById('text');
+
+    text.innerHTML = 'This is really cool!';
+  });
